13bc5069b26cfdc98bbbc5d594a7de30a17b0bda
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-22 15:30:42 -1000

    step 13 completed

--- todo_app.sql
+++ todo_app.sql
@@ -1,6 +1,6 @@
 DROP USER IF EXISTS micheal;
 
-CREATE USER micheal WITH ENCRYPTED_PASSWORD 'stonebreaker';
+CREATE USER micheal WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 DROP DATABASE IF EXISTS todo_app;
 
@@ -8,14 +8,26 @@ CREATE DATABASE todo_app;
 
 \connect todo_app
 
+CREATE SEQUENCE auto_incrementing start 1 increment 1;
+
 CREATE TABLE tasks (
 
-  id INTEGER,
-  title VARCHAR(45) NOT NULL,
-  description VARCHAR(150),
-  created_at = time without timezone NOT NULL DEFAULT NOW(),
-  updated_at = time without timezone NOT NULL DEFAULT NOW(),
-  completed_at = time DEFAULT NULL,
+  id INTEGER NOT NULL DEFAULT nextval('auto_incrementing'),
+  title VARCHAR(255) NOT NULL,
+  description text,
+  created_at time without time zone NOT NULL DEFAULT NOW(),
+  updated_at time without time zone,
+  completed boolean NOT NULL DEFAULT false,
   CONSTRAINT tasks_pk PRIMARY KEY (id)
 
 );
+
+ALTER TABLE tasks DROP COLUMN completed;
+ALTER TABLE tasks ADD COLUMN completed_at time without time zone DEFAULT NULL;
+ALTER TABLE tasks ALTER updated_at SET DEFAULT NOW();
+ALTER TABLE tasks ALTER updated_at SET NOT NULL;
+
+INSERT INTO tasks VALUES (0, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+INSERT INTO tasks (title, description) VALUES ('Study PostgreSQL', 'Read all the documentation');
+
